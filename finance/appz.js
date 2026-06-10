2c5054972cacb530c676f944db6cf81ec0704bda
1upkr <aka.herlock@gmail.com>
2026-06-11 08:49:49 +0900

    Update appz.js

--- finance/appz.js
+++ finance/appz.js
@@ -8,7 +8,7 @@ const DEFAULT_WATCHLISTS = {
 };
 
 const GAS_PROXY_URL = "https://script.google.com/macros/s/AKfycbxc8Q5iI7WxZurtV-1FDjTWKPUx_i049HSBAap2AyKYSvs8QMRHD3ZTa3xqfu0tJ1Za/exec";
-const NAVER_GAS_PROXY_URL = "https://script.google.com/macros/s/AKfycbzz2RugFLCI1odF6xQ1VvReSYsIveMeMwH1EfEze69OEfLdyVEyQWW3Q8KEBSfH1DG_6w/exec"; 
+const NAVER_GAS_PROXY_URL = "https://script.google.com/macros/s/AKfycbwDfZb4Y-R6G1Vjx6K_qYjxulBh7arbehO5CEC98psKVX6X8RyjR2WLxhdULWeY_qcr2Q/exec"; 
 const NEWS_GAS_PROXY_URL = "https://script.google.com/macros/s/AKfycbwSD8MOLPrYjwTBVQX_Tq6pu-gTHlOeR7p0hUY2pHGACNc2NA6f4zICduC05ypO_EN6/exec"; 
 
 const CHO_HANGUL = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
