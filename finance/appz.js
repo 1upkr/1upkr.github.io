e2dbfa05aa27a89e80e40d3f7137572564a27feb
1upkr <aka.herlock@gmail.com>
2026-06-15 08:58:24 +0900

    Update appz.js

--- finance/appz.js
+++ finance/appz.js
@@ -10,7 +10,7 @@ const DEFAULT_WATCHLISTS = {
 const GAS_PROXY_URL = "https://script.google.com/macros/s/AKfycbxc8Q5iI7WxZurtV-1FDjTWKPUx_i049HSBAap2AyKYSvs8QMRHD3ZTa3xqfu0tJ1Za/exec";
 const NAVER_GAS_PROXY_URL = "https://script.google.com/macros/s/AKfycbymIKO0njjKUqFvgcjY39To9E2rkJMCqbwL0uWUjyyvVQREn6foLLdI44rVnoehvi6Ztg/exec"; 
 const NEWS_GAS_PROXY_URL = "https://script.google.com/macros/s/AKfycbwSD8MOLPrYjwTBVQX_Tq6pu-gTHlOeR7p0hUY2pHGACNc2NA6f4zICduC05ypO_EN6/exec"; 
-const TREND_GAS_PROXY_URL = "https://script.google.com/macros/s/AKfycbyV8COzKSWjJjeKTNZs-cixUJquW3TucUWpAWjTWqt4cIcsy_jFFyxDHQ-9J3nsMVlXIA/exec";
+const TREND_GAS_PROXY_URL = "https://script.google.com/macros/s/AKfycbweuE_ty_22Nr8rwCR6ZmOawW-q_240KOv2d8eGfDNPrGWKP8oh5jaqr8e9FEYeSFfE_w/exec";
 
 const CHO_HANGUL = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
 function getChosung(str) {
