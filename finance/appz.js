863a5894bdedc953653a2f3f67ed561d3b0da842
1upkr <aka.herlock@gmail.com>
2026-09-18 15:03:02 +0900

    Update NEWS_GAS_PROXY_URL to new endpoint

--- finance/appz.js
+++ finance/appz.js
@@ -10,7 +10,7 @@ const DEFAULT_WATCHLISTS = {
 const YAHOO_FINANCE_PROXY_URL = "https://script.google.com/macros/s/AKfycbxzBxcv3llVtcWyWoldEHkpyNBgWSn0D2_j_SYLEf6r-a4zMZ4VpOE6cxtwPQw_wvKt6Q/exec";
 const NAVER_FINANCE_PROXY_URL = "https://script.google.com/macros/s/AKfycbyXf76mrHHn3F5_ZEO8i813IyPv3e24f7K8B7N16cKNfZo1D5seaeUBOhtsyK_ciuBwjQ/exec"; 
 const TREND_CHART_GAS_PROXY_URL = "https://script.google.com/macros/s/AKfycby4YZ1sOdQPfde-nrzAN0vUjhRP1Phn9C1ppFY2m8YHywGz-7GhNcHLU19PFCLeqm3u/exec";
-const NEWS_GAS_PROXY_URL = "https://script.google.com/macros/s/AKfycbwQrqLXHp9RnzgxWxcsYNXdqBf0CtbwpJHGIZw1o0Hfc6M38QoyVzJSrYahqtfg5MIQ/exec"; 
+const NEWS_GAS_PROXY_URL = "https://script.google.com/macros/s/AKfycbwqx9uqCj8gEkuxkSY6Q6ccZzV_2_oghNXXY8K09mxS2qmc1m9JtxJe0_4kic8TmgH-/exec"; 
 const KNIGHT_GAS_PROXY_URL = "https://script.google.com/macros/s/AKfycbxNL4-6PqMSqylMQBP0CdqSKS0LYEK7Yn7tbFtiuIfbKlQGcAanznYX85r0CpxQ8J1f_Q/exec";
 
 const CHO_HANGUL = ['ㄱ', 'ㄲ', 'ㄴ', 'ㄷ', 'ㄸ', 'ㄹ', 'ㅁ', 'ㅂ', 'ㅃ', 'ㅅ', 'ㅆ', 'ㅇ', 'ㅈ', 'ㅉ', 'ㅊ', 'ㅋ', 'ㅌ', 'ㅍ', 'ㅎ'];
